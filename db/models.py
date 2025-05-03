8cb5cd80a8a3b3ea7d94b213a1a34eb505bd4b64
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-05-03 14:12:39 +0200

    feat: create game models

--- db/models.py
+++ db/models.py
@@ -1,1 +1,73 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    @classmethod
+    def get_or_create(cls, race_dict: dict) -> "Race":
+        try:
+            return cls.objects.get(name=race_dict["name"])
+        except ObjectDoesNotExist:
+            return cls.objects.create(
+                name=race_dict["name"],
+                description=race_dict["description"]
+            )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.TextField()
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    @classmethod
+    def get_or_create(cls, **kwargs) -> "Skill":
+        try:
+            return cls.objects.get(name=kwargs["name"])
+        except ObjectDoesNotExist:
+            return cls.objects.create(
+                name=kwargs["name"],
+                bonus=kwargs["bonus"],
+                race=kwargs["race"]
+            )
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.race.name}"
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    @classmethod
+    def get_or_create(cls, guild_dict: dict) -> "Guild" or None:
+        if guild_dict:
+            try:
+                return cls.objects.get(name=guild_dict["name"])
+            except ObjectDoesNotExist:
+                return cls.objects.create(
+                    name=guild_dict["name"],
+                    description=guild_dict["description"]
+                )
+
+        return None
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.nickname} {self.race.name}"
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import init_django_orm  # noqa: F401
-
+import json
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json") as json_players_file:
+        players = json.load(json_players_file)
+
+    if players:
+        for name, player in players.items():
+            player_race = Race.get_or_create(player["race"])
+            player_guild = Guild.get_or_create(player["guild"])
+
+            for skill in player["race"]["skills"]:
+                Skill.get_or_create(
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=player_race
+                )
+
+            Player.objects.create(
+                nickname=name,
+                email=player["email"],
+                bio=player["bio"],
+                race=player_race,
+                guild=player_guild
+            )
 
 
 if __name__ == "__main__":
